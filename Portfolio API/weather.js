d1d046f2a839e4719a4e9e2414b297902ba66169
Luke Edwards-Sparsholtt <87570488+LukeES23@users.noreply.github.com>
2023-10-22 16:10:44 +0100

    Update weather.js v3

--- Portfolio API/weather.js
+++ Portfolio API/weather.js
@@ -1,20 +1,30 @@
 // Get a reference to the weather map container
-const mapContainer = L.map('weather-map').setView([51.505, -0.09], 5); // Set the initial view
+const mapContainer = L.map('weather-map').setView([54.702, -3.276], 6); // Set the initial view
 
 // Add a tile layer to the map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
 }).addTo(mapContainer);
 
-// Fetch weather data from the API (replace with the actual API endpoint)
-const apiUrl = 'YOUR_WEATHER_API_ENDPOINT';
+// Define the location for which you want to fetch weather data (e.g., London, UK)
+const city = 'London';
+const country = 'UK';
+
+// Replace 'YOUR_OPENWEATHERMAP_API_KEY' with your actual OpenWeatherMap API key
+const apiKey = 'adb3403328844831978164251a5ce75b';
+
+// Construct the API URL to fetch weather data
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`;
+
+// Fetch weather data from the OpenWeatherMap API
 fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
         // Process the weather data and add markers or layers to the map
-        // Example: Loop through data and add markers for each location
-        data.locations.forEach((location) => {
-            L.marker([location.lat, location.lon]).addTo(mapContainer)
-                .bindPopup(`Temperature: ${location.temperature}&deg;C<br>${location.weatherDescription}`);
-        });
+        // Example: Display temperature and description on the map
+        const temperature = data.main.temp;
+        const weatherDescription = data.weather[0].description;
+
+        L.marker([54.702, -3.276]).addTo(mapContainer)
+            .bindPopup(`Temperature: ${temperature}&deg;C<br>${weatherDescription}`);
     });
